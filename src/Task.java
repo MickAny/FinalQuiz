416ceac9cad3b7cd13a607ce6cddd7ee5af5714d
Mikhail <aniskevich_mikhail@mail.ru>
2023-04-18 20:42:31 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,17 @@
+public class Task
+{
+    public static void main(String[] args)
+    {
+
+    }
+    static void SecondArrayWithIF( String[] arr1, String[]  arr2)
+    {
+
+    }
+
+    static void PrintArray(String[] array)
+    {
+
+    }
+
+}
