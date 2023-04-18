36599abbcd9a67bb296f49244f4ebe8048b25346
Mikhail <aniskevich_mikhail@mail.ru>
2023-04-18 20:45:22 +0300

    Third commit

--- src/Task.java
+++ src/Task.java
@@ -2,7 +2,16 @@ public class Task
 {
     public static void main(String[] args)
     {
+        //String[] arr1 = new String[]{"Russia", "Denmark", "Kazan"};
+        //String[] arr1 = new String[]{"1234", "1567", "-2", "computer science"};
+        //String[] arr1 = new String[]{"Hello", "2", "world", ":-)"};
 
+        String[] arr1 = new String[]{"=)", "Nevermind", "hey", "world", "BrB", "False", "-3"};
+        String[] arr2 = new String[arr1.length];
+
+        SecondArrayWithIF(arr1, arr2);
+        System.out.println("Only elements less than four");
+        PrintArray(arr2);
     }
     static void SecondArrayWithIF( String[] arr1, String[]  arr2)
     {
