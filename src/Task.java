d4275b0c4db42c8d860489e9bd1f76bcb30f93cf
Mikhail <aniskevich_mikhail@mail.ru>
2023-04-18 20:44:21 +0300

    Second commit

--- src/Task.java
+++ src/Task.java
@@ -6,12 +6,23 @@ public class Task
     }
     static void SecondArrayWithIF( String[] arr1, String[]  arr2)
     {
-
+        int count = 0;
+        for (int i = 0; i < arr1.length; i++)
+        {
+            if(arr1[i].length() <= 3)
+            {
+                arr2[count] = arr1[i];
+                count++;
+            }
+        }
     }
 
     static void PrintArray(String[] array)
     {
-
+        for (int i = 0; i < array.length; i++)
+        {
+            if(array[i] != null) System.out.println(array[i]);
+        }
     }
 
 }
